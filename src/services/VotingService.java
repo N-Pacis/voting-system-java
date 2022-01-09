0f3a95e54128d288df0c7315603bd5ae7a59b223
Nkubito Pacis <pacisnkubito@gmail.com>
2022-01-09 20:23:59 +0200

    [Feat] changed fileOperations to votingService

---
+++ src/services/VotingService.java
@@ -0,0 +1,43 @@
+package services;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VotingService {
+    private static FileInputStream inputStream;
+    private static FileOutputStream outputStream;
+
+    public List<String> readFileData(String filename) throws IOException {
+        inputStream = new FileInputStream("storage/"+filename);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        Integer count = 0;
+        List<String> Candidates = new ArrayList<String>();
+        while(reader.ready()){
+            count++;
+            String line = reader.readLine();
+            String[] splittedLine = line.split(",");
+            Candidates.add(splittedLine[0]);
+        }
+        return Candidates;
+    }
+
+    public List<String> readWholeFileData(String filename) throws IOException {
+        inputStream = new FileInputStream("storage/"+filename);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        Integer count = 0;
+        List<String> Candidates = new ArrayList<String>();
+        while(reader.ready()){
+            count++;
+            String line = reader.readLine();
+            Candidates.add(line);
+        }
+        return Candidates;
+    }
+
+    public void saveToFile(String filename, String data,Boolean append) throws IOException{
+        outputStream = new FileOutputStream("storage/"+filename, append);
+        byte[] bytesArray = data.getBytes();
+        outputStream.write(bytesArray);
+    }
+}
