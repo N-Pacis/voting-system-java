2e5b6ddb5c90a6df2d9759084042f197e61ea294
Nkubito Pacis <pacisnkubito@gmail.com>
2022-01-09 22:25:43 +0200

    [Fix] removed unnecessary breaks

--- src/com/votingSystem/Main.java
+++ src/com/votingSystem/Main.java
@@ -7,19 +7,21 @@ public class Main {
     private static Integer choice;
     public static void main(String[] args) throws IOException {
 	    do{
-            System.out.println("##################################################\n");
-            System.out.println(" ###### Welcome to Election/Voting 2022 #####\n");
+            System.out.println("##################################################");
+            System.out.println(" ###### Welcome to Election/Voting 2022 #####");
             System.out.println("### CHOOSE AN ACTION BELOW ###");
-            System.out.println("\n\t\t\t\t 1. Cast your vote");
-            System.out.println("\n\t\t\t\t 2. Find Vote Count");
-            System.out.println("\n\t\t\t\t 3. Find the leading candidate");
-            System.out.println("\n\t\t\t\t 0. Exit");
-            System.out.println("\n PLEASE ENTER YOUR CHOICE:\n");
+            System.out.println("\t\t\t\t 1. Cast your vote");
+            System.out.println("\t\t\t\t 2. Find Vote Count");
+            System.out.println("\t\t\t\t 3. Find the leading candidate");
+            System.out.println("\t\t\t\t 4. Register to the system");
+            System.out.println("\t\t\t\t 0. Exit");
+            System.out.println("PLEASE ENTER YOUR CHOICE:");
 
             Scanner scan = new Scanner(System.in);
             choice = scan.nextInt();
 
             Voting voting = new Voting();
+            User user = new User();
             switch(choice){
                 case 1:
                     voting.castVote();
@@ -29,6 +31,9 @@ public class Main {
                     break;
                 case 3:
                     voting.leadingCandidate();
+                    break;
+                case 4:
+                    user.registerUser();
                     break;
                 case 0:
                     System.exit(0);
