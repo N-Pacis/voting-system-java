0790e40cdd85b24b2bb0cc19259817e1bc84f947
Nkubito Pacis <pacisnkubito@gmail.com>
2022-01-09 22:26:27 +0200

    [Fix] removed the try and catch block and added a method of generating inputs

--- src/com/votingSystem/User.java
+++ src/com/votingSystem/User.java
@@ -3,6 +3,7 @@ package com.votingSystem;
 import exceptions.EmailExistsException;
 import exceptions.InvalidUserException;
 import services.UserService;
+import validators.UserValidator;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -11,7 +12,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class User {
-    private static UserService userService = new UserService();
+    UserValidator validator = new UserValidator();
     private static Scanner scan = new Scanner(System.in);
 
     private static String name;
@@ -19,49 +20,45 @@ public class User {
     private static String nationality;
     private static String email;
     private static String password;
+    private static List<String> invalidFields = new ArrayList<String>();
 
-    public User() {}
-    public User(String name, String nationalId, String nationality, String email, String password) throws InvalidUserException, EmailExistsException {
-        if(name.length() < 5){
-            throw new InvalidUserException("Your name must be at least 5 characters");
-        }
-        if(name.split(" ").length < 2){
-            throw new InvalidUserException("You must provide your full names");
-        }
-        if(!(nationalId.length() == 16)){
-            throw new InvalidUserException("You must provide a valid national id");
-        }
-        String[] splitted = email.split("@");
-        List<String> emailSplitted = new ArrayList<String>(Arrays.asList(splitted));
-
-        if(email.length() < 5 || !emailSplitted.contains("gmail.com")){
-            throw new InvalidUserException("Invalid email address");
-        }
-        if(userService.checkEmail(email)){
-            throw new EmailExistsException("Email Already Exists");
-        }
-        if(password.length() < 5){
-            throw  new InvalidUserException("Password must be atleast 5 characters");
-        }
-
-    }
     public void registerUser() throws IOException{
+        String[] fields= {"name","nationalId","nationality","email","password"};
         System.out.println("### \t\tPLEASE FILL THE REQUIRED INFORMATION ###");
-        System.out.println("\t\t - ENTER YOUR NAME:");
-        name = scan.nextLine();
-        System.out.println("\t\t - ENTER YOUR NATIONAL ID");
-        nationalId = scan.nextLine();
-        System.out.println("\t\t - ENTER YOUR NATIONALITY");
-        nationality = (scan.nextLine()).toUpperCase();
-        System.out.println("\t\t - ENTER YOUR EMAIL");
-        email = scan.nextLine().toLowerCase();
-        System.out.println("\t\t - ENTER YOUR PASSWORD");
-        password = scan.nextLine();
-        try{
-            new User(name,nationalId,nationality,email,password);
+        for(String field : fields){
+            generateInput(field);
         }
-        catch(InvalidUserException | EmailExistsException Ex){
-            System.out.println(Ex.getMessage());
+        System.out.println("Name: "+name);
+        System.out.println("nationalId: "+nationalId);
+        System.out.println("nationality: "+nationality);
+        System.out.println("email: "+email);
+        System.out.println("Password: "+password);
+    }
+
+    private void generateInput(String fieldName){
+        System.out.println("\t\t - ENTER YOUR "+fieldName.toUpperCase());
+        switch(fieldName){
+            case "name":
+                name = scan.nextLine();
+                name = validator.checkName(name);
+                break;
+            case "nationalId":
+                nationalId = scan.nextLine();
+                nationalId = validator.checkNationalId(nationalId);
+                break;
+            case "nationality":
+                nationality = (scan.nextLine()).toUpperCase();
+                break;
+            case "email":
+                email = scan.nextLine().toLowerCase();
+                email = validator.checkEmail(email);
+                break;
+            case "password":
+                password = scan.nextLine();
+                password = validator.checkPassword(password);
+                break;
+            default:
+                System.out.println("Invalid field");
         }
     }
 }
