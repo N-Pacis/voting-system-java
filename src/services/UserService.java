b0e1977662e40206ef178c2ade1d7a2d7b42ff86
Nkubito Pacis <pacisnkubito@gmail.com>
2022-01-12 09:02:31 +0200

    [Feat] added checkCredentials method to check if the provided credentials exists in user files

--- src/services/UserService.java
+++ src/services/UserService.java
@@ -1,12 +1,31 @@
 package services;
 
-import exceptions.EmailExistsException;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class UserService extends MainService{
+    private static FileInputStream inputStream;
+
     public Boolean checkEmail(String email){
         if(email.equals("abcd@gmail.com")){
             return true;
         }
         return false;
     }
+
+    public Boolean checkCredentials(String email,String password) throws IOException {
+        inputStream = new FileInputStream("storage/users.csv");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        while(reader.ready()){
+            String line = reader.readLine();
+            String[] splittedLine = line.split(",");
+            if(splittedLine[2].equals(email) && splittedLine[3].equals(password)){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
