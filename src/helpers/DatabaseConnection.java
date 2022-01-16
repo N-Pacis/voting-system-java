411c8fb73accefd66df3cc511c48de471a8fa442
Nkubito Pacis <pacisnkubito@gmail.com>
2022-01-16 12:06:57 +0200

    Update DatabaseConnection.java

--- src/helpers/DatabaseConnection.java
+++ src/helpers/DatabaseConnection.java
@@ -3,14 +3,14 @@ package helpers;
 import java.sql.*;
 
 public class DatabaseConnection {
-    private static String dbUrl = "jdbc:mysql//localhost:3306/votingSystem";
+    private static String dbUrl = "jdbc:mysql://localhost/votingSystem";
     private static String dbUser = "root";
     private static String dbPassword = "pacis123";
+    public static Connection myConnection;
 
     public DatabaseConnection() {
         try{
-            Connection myConnection = DriverManager.getConnection(dbUrl,dbUser,dbPassword);
-            Statement myStatement = myConnection.createStatement();
+            myConnection = DriverManager.getConnection(dbUrl,dbUser,dbPassword);
         }
         catch(Exception ex){
             System.out.println(ex.getMessage());
